8ba77a83ce424ad1c2b9bfe768a320201005dd02
Ibodullo <ibodzodaibodullo@gmail.com>
2021-04-24 01:16:28 +0500

    + added car removing functionality

--- app/carsTable/carsTable.js
+++ app/carsTable/carsTable.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const carsTable = angular.module('myApp.carsTable', ["agGrid"])
+    .component('carsTable', {
+        templateUrl: 'carsTable/carsTable.html',
+    });
+
 const cars = [
     {
         make: 'Toyota',
@@ -66,27 +71,34 @@ const columnDefs = [
     {headerName: "Make", field: "make", filter: true, editable: true},
     {headerName: "Model", field: "model", filter: true, editable: true},
     {headerName: "Price", field: "price", filter: true, editable: true},
-    {
-        headerName: "Action",
-        field: "action",
-        cellRenderer: (params) => '<div class="float-end"><button ng-click="editCarHandler(params)" class="btn btn-outline-secondary btn-sm"><i class="fas fa-pencil-alt"></i></button><button ng-click="deleteCarHandler(params)" style="margin-left: 10px" class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i></button></div>'
-    },
 ];
 
-const carsTable = angular.module('myApp.carsTable', ["agGrid"])
-    .component('carsTable', {
-        templateUrl: 'carsTable/carsTable.html',
-    });
-
 carsTable.controller("carsTableCtrl", function ($scope) {
+
     $scope.gridOptions = {
         columnDefs: columnDefs,
         rowData: cars,
         pagination: true,
         suppressCellSelection: true,
         paginationAutoPageSize: true,
+        rowSelection: 'single',
         onGridReady: function (params) {
             params.api.sizeColumnsToFit();
         }
+    };
+    $scope.isRowSelected = false;
+
+    document.addEventListener('click', function (event) {
+        if(event.target.className.substring(0, 7) === 'ag-cell') {
+            $scope.isRowSelected = true;
+            $scope.$apply();
+        }
+    });
+
+    $scope.deleteRow = function deleteRow() {
+        const selected = $scope.gridOptions.api.getFocusedCell();
+        $scope.gridOptions.rowData.splice(selected.rowIndex, 1);
+        $scope.gridOptions.api.setRowData($scope.gridOptions.rowData);
+        $scope.isRowSelected = false;
     };
 });
